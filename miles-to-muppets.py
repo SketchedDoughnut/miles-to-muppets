fec54eac9904a3d3c1563edc5e2509d6d175e2db
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-05-19 22:05:04 -0700

    better

--- miles-to-muppets.py
+++ miles-to-muppets.py
@@ -71,10 +71,9 @@ def get_auth_header(token: str) -> dict:
 token = get_token()
 auth_header = get_auth_header(token)
 print('-----------------------------')
-print("DATA:")
+print("SESSION DATA:")
 print("Token:", token)
 print("Auth header:", auth_header)
-print('-----------------------------')
 
 # establish song dict data
 dat = data
@@ -88,57 +87,62 @@ constants = dat['constants']
 speed = constants['speed']
 minPerMile = constants['minPerMile']
 
-# get destination info (use google API later?)
-distance = int(input('How far is your destination, in miles? \n--> '))
+while True:
+    # get destination info (use google API later?)
+    print('-----------------------------')
+    distance = int(input('How far is your destination, in miles? \n--> '))
 
-# get album choice
-print('-----------------------------')
-print('Please choose from the following albums:')
-for key, album in zip(key_iter, album_iter):
-    print(f"- {key}: {album}")
-choice = int(input('--> '))
-album_id = album_list[key_list[choice]]
-print('-----------------------------')
+    # get album choice
+    print('-----------------------------')
+    print('Please choose from the following albums:')
+    for key, album in zip(key_iter, album_iter):
+        print(f"- {key}: {album}")
+    choice = int(input('--> '))
+    album_id = album_list[key_list[choice]]
+    print('-----------------------------')
 
-album_data = requests.get(f'https://api.spotify.com/v1/albums/{album_id}', headers=auth_header).json()
-album_name = album_data['name']
-print('Album name:', album_name)
-print('Total amount of songs:', album_data['total_tracks'])
-# print('Album type:', album_data['album_type'])
-# print('Base keys:', album_data.keys())
-# print('Track keys:', album_data['tracks'].keys())
-# print('Track items:', album_data['tracks']['items'])
+    album_data = requests.get(f'https://api.spotify.com/v1/albums/{album_id}', headers=auth_header).json()
+    album_name = album_data['name']
+    print('Album name:', album_name)
+    print('Total amount of songs:', album_data['total_tracks'])
+    # print('Album type:', album_data['album_type'])
+    # print('Base keys:', album_data.keys())
+    # print('Track keys:', album_data['tracks'].keys())
+    # print('Track items:', album_data['tracks']['items'])
 
-# start loop
-print('-----------------------------')
-input('Enter anything to start evaluation...')
+    # start loop
+    # print('-----------------------------')
+    # input('Enter anything to start evaluation...')
 
-total_time = 0
-song_amount = 0
-found_max = False
-mileTominLength = minPerMile * distance
-mileToMsLength = (mileTominLength * 60) * 1000
+    total_time = 0
+    song_amount = 0
+    found_max = False
+    mileTominLength = minPerMile * distance
+    mileToMsLength = (mileTominLength * 60) * 1000
 
-print("-----------------------------\n ")
-for track in album_data['tracks']['items']:
-    name = track['name']
-    duration_ms = track['duration_ms']
-    leng = "                                                                           " * 2
-    sys.stdout.write("\033[F")
-    sys.stdout.write(f"{leng}\n{leng}")
-    sys.stdout.write("\033[F")
-    sys.stdout.write(f"\rsong name: {name}\nduration: {duration_ms}ms")
-    sys.stdout.flush()
-    time.sleep(0.1)
-    
-    total_time += duration_ms
-    song_amount += 1
-    if total_time >= mileToMsLength:
-        found_max = True
+    print("-----------------------------\n ")
+    for track in album_data['tracks']['items']:
+        name = track['name']
+        duration_ms = track['duration_ms']
+        leng = "                                                                           " * 2
+        sys.stdout.write("\033[F")
+        sys.stdout.write(f"{leng}\n{leng}")
+        sys.stdout.write("\033[F")
+        sys.stdout.write(f"\rsong name: {name}\nduration: {duration_ms}ms")
+        sys.stdout.flush()
+        time.sleep(0.1)
+        
+        total_time += duration_ms
+        song_amount += 1
+        if total_time >= mileToMsLength:
+            found_max = True
+            break
+    print(" ")
+    print('-----------------------------')
+    if found_max:
+        print(f'At an average speed of {speed}mph, taking {minPerMile} minute per mile, it will take you roughly {song_amount} muppet songs from the album "{album_name}" to reach your destination.')
+    else:
+        print(f'At an average speed of {speed}mph, taking {minPerMile} minute per mile, you would have completed this album ({song_amount} songs) on the trip to your destination.')
+        print(f'There would be {round(((mileToMsLength - total_time) / 1000) / 60, 2)} minutes left.') 
+    if input('Continue? (y/n) ').lower() == 'n':
         break
-print(" ")
-print('-----------------------------')
-if found_max:
-    print(f'At an average speed of {speed}mph, taking {minPerMile} minute per mile, it will take you roughly {song_amount} muppet songs from the album "{album_name}" to reach your destination.')
-else:
-    print(f'At an average speed of {speed}mph, taking {minPerMile} minute per mile, you would have completed this album ({song_amount} songs) on the trip to your destination.')
