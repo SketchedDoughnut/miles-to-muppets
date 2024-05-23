de52ac2afdee0d22a9ac018bd9e4d4dc2c0d492b
Sketched Productions <163922554+SketchedProductions@users.noreply.github.com>
2024-05-23 21:09:43 +0000

    save

--- testrun.py
+++ testrun.py
@@ -1,12 +1,12 @@
-from src import milesToMuppets as muppet
+import milesToMuppets as muppet
 
 foo = muppet.MilesToMuppets(
     client_id='x',
-    client_secret='x'
+    client_secret='x' 
 )
 
 foo.set_mile_distance(60)
 foo.set_speed(30)
 albums = foo.get_albums()
 foo.set_album(0)
-foo.evaluate_album()
+foo.evaluate_album(print_cycle=True)
