a871369cffb9bba3c0b725001599e4be2a8fd1e2
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-06-06 20:47:58 -0700

    Update testrun.py

--- testrun.py
+++ testrun.py
@@ -2,8 +2,8 @@
 from src import milesToMuppets as muppet
 
 foo = muppet.MilesToMuppets(
-    client_id='0c8bb718abf34741b5378e4c2e5fd306',
-    client_secret='09cf579bfdf24781adf80a251e0f6628' 
+    client_id=input('-> '),
+    client_secret=input('-> ')
 )
 
 foo.set_mile_distance(60)
